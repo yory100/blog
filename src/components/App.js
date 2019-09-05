91c7bb8a8e5ae8fb716320021b883318b6e4dd6d
Yordan Georgiev Yordanov <yyord@softserveinc.com>
2019-09-05 15:38:33 +0300

    commit end

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="ui container">
+      <PostList />
     </div>
   );
 }
